7e66c3df662a85aa56c4f2175eda3906a917639c
spudro <1488228>
2022-06-13 23:18:38 +0300

    Первая сущность и миграция

--- src/Domain/Entity/Review.php
+++ src/Domain/Entity/Review.php
@@ -7,11 +7,26 @@ namespace App\Domain\Entity;
 
 use App\Domain\VO\StarsQuantity;
 use App\Domain\VO\UserId;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity]
+#[ORM\Table(name: "reviews")]
 class Review
 {
     #[\ApiPlatform\Core\Annotation\ApiProperty(identifier: true)]
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    #[ORM\GeneratedValue]
     public ?int $id = null;
+
+    #[ORM\Column(type: "string")]
+    private string $comment;
+
+//    #[ORM\Embedded(class: StarsQuantity::class, columnPrefix: false)]
+//    private StarsQuantity $startQ;
+//
+//    #[ORM\Embedded(class: UserId::class, columnPrefix: false)]
+//    private ?UserId $userId;
 
     public function __construct(
         string        $comment,
@@ -19,6 +34,9 @@ class Review
         ?UserId       $userId = null,
     )
     {
+        $this->comment = $comment;
+        $this->startQ = $startQ;
+        $this->userId = $userId;
     }
 
 
